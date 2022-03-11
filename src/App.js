34e04d2f533458358b21314da5fe01befead1272
unknown <soumy99sharma@gmail.com>
2022-03-11 20:45:37 +0530

    Basic starting point of react project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
-import Welcome from './pages/Welcome';
-import Products from './pages/Products';
-import MainHeader from './components/MainHeader';
 function App() {
   return (
-    <React.Fragment>
-      <header>
-        <MainHeader />
-      </header>
-      <main>
-        <Route path="/welcome">
-          <Welcome />
-        </Route>
-        <Route path="/products">
-          <Products />
-        </Route>
-      </main>
-    </React.Fragment>
+    <div>
+      <h2>Let's get started!</h2>
+    </div>
   );
 }
 

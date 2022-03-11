3d65e02798b46cc7da79cf6764c0cde9ec6a3f0c
unknown <soumy99sharma@gmail.com>
2022-03-12 00:04:56 +0530

    Step-5: Getting dynamic content info from url

--- src/pages/ProductDeatils.js
+++ src/pages/ProductDeatils.js
@@ -1,4 +1,10 @@
+import { useParams } from "react-router";
+
 const ProductDetails = (props) => {
-  return <h1>Product Details</h1>;
+  const params = useParams();
+  return (<section>
+    <h1>Product Details</h1>
+    <p>{params.productId}</p>
+  </section>)
 };
 export default ProductDetails;
